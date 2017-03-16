b8e210e47424882423c828ae250dfba08afdb2d2
Ramiz Raja <informramiz@gmail.com>
2017-03-16 20:29:19 +0500

    feat: first implementation of kalman filter to localize vehicle
    This implementation includes prediction and measurement update steps designed specific for localizing a vehicle. For example given measurements  x = [1, 2, 3] it update and predicts position for reach step and also calculates velocity.

---
+++ kalmanFilter/kalmanFilter/main.cpp
@@ -0,0 +1,105 @@
+//
+//  main.cpp
+//  kalmanFilter
+//
+//  Created by Ramiz Raja on 3/15/17.
+//  Copyright © 2017 Ramiz Raja. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+#include "Eigen/Dense"
+
+using namespace std;
+using namespace Eigen;
+
+//Kalman Filter Variables
+VectorXd x;
+MatrixXd P;
+VectorXd u;
+MatrixXd F;
+MatrixXd H;
+MatrixXd R;
+MatrixXd I;
+
+//measurements
+vector<VectorXd> measurements;
+
+void predict(VectorXd &x, VectorXd &P);
+void update(VectorXd &x, MatrixXd &P, VectorXd z);
+void filter(VectorXd &x, MatrixXd &P);
+void initMatrices();
+void initMeasurements();
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    initMatrices();
+    initMeasurements();
+    
+    filter(x, P);
+    cout << "Final x: " << endl << x << endl;
+    cout << "Final P: " << endl << P << endl;
+    
+    return 0;
+}
+
+void initMeasurements() {
+    VectorXd single_measurement(1);
+    
+    single_measurement << 1;
+    measurements.push_back(single_measurement);
+    
+    single_measurement << 2;
+    measurements.push_back(single_measurement);
+    
+    single_measurement << 3;
+    measurements.push_back(single_measurement);
+}
+
+void initMatrices() {
+    x = VectorXd(2);
+    x << 0, 0;
+    
+    P = MatrixXd(2, 2);
+    P << 1000, 0, 0, 100;
+    
+    u = VectorXd(2);
+    u << 0, 0;
+    
+    F = MatrixXd(2, 2);
+    F << 1, 1, 0, 1;
+    
+    H = MatrixXd(1, 2);
+    H << 1, 0;
+    
+    R = MatrixXd(1, 1);
+    R << 1;
+    
+    I = MatrixXd::Identity(2, 2);
+}
+
+void predict(VectorXd &x, MatrixXd &P) {
+    x = F * x + u;
+    P = F * P * F.transpose();
+}
+
+void update(VectorXd &x, MatrixXd &P, VectorXd z) {
+    VectorXd y = z - H * x;
+    VectorXd S = H * P * H.transpose() + R;
+    VectorXd K = P * H.transpose() * S.inverse();
+    x = x + (K * y);
+    P = (I - K * H) * P;
+}
+
+void filter(VectorXd &x, MatrixXd &P) {
+    for (unsigned int i = 0; i < measurements.size(); ++i) {
+        VectorXd z = measurements[i];
+        //measurement update step
+        update(x, P, z);
+        //prediction step
+        predict(x, P);
+    }
+}
