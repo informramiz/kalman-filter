fc75f9ca2e64a6a8c39bde6cca6986767f02fff5
Ramiz Raja <informramiz@gmail.com>
2017-03-17 05:14:25 +0500

    Refactor: Coded Tracking header/source files, an encapsulation of KF

---
+++ kalmanFilter/kalmanFilter/Tracking.cpp
@@ -0,0 +1,9 @@
+//
+//  Tracking.cpp
+//  kalmanFilter
+//
+//  Created by Ramiz Raja on 3/17/17.
+//  Copyright © 2017 Ramiz Raja. All rights reserved.
+//
+
+#include "Tracking.hpp"
---
+++ kalmanFilter/kalmanFilter/Tracking.hpp
@@ -0,0 +1,14 @@
+//
+//  Tracking.hpp
+//  kalmanFilter
+//
+//  Created by Ramiz Raja on 3/17/17.
+//  Copyright © 2017 Ramiz Raja. All rights reserved.
+//
+
+#ifndef Tracking_hpp
+#define Tracking_hpp
+
+#include <stdio.h>
+
+#endif /* Tracking_hpp */
