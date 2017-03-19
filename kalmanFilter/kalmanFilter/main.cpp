9b4ee06629a6aa574950f1af2a567e1d3e1aac3c
Ramiz Raja <informramiz@gmail.com>
2017-03-19 05:11:10 +0500

    feat: RMSE function to Evaluate Extended Kaman Filter Performance

--- kalmanFilter/kalmanFilter/main.cpp
+++ kalmanFilter/kalmanFilter/main.cpp
@@ -26,19 +26,82 @@ using Eigen::VectorXd;
 vector<MeasurementPackage> readMeasurements();
 void runKalmanFilter(const vector<MeasurementPackage> & measurement_pack_list);
 Eigen::MatrixXd calculateJacobian(const VectorXd& x_state);
+VectorXd calculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
 
 int main(int argc, const char * argv[]) {
     
 //    vector<MeasurementPackage> measurement_pack_list = readMeasurements();
 //    runKalmanFilter(measurement_pack_list);
     
-    Eigen::VectorXd x_state(4);
-    x_state << 1, 2, 0.2, 0.4;
-    MatrixXd Hj = calculateJacobian(x_state);
-    cout << "Hj: " << endl;
-    cout << Hj << endl;
+//    Eigen::VectorXd x_state(4);
+//    x_state << 1, 2, 0.2, 0.4;
+//    MatrixXd Hj = calculateJacobian(x_state);
+//    cout << "Hj: " << endl;
+//    cout << Hj << endl;
+    
+    /*
+     * Compute RMSE
+     */
+    vector<VectorXd> estimations;
+    vector<VectorXd> ground_truth;
+    
+    //the input list of estimations
+    VectorXd e(4);
+    e << 1, 1, 0.2, 0.1;
+    estimations.push_back(e);
+    e << 2, 2, 0.3, 0.2;
+    estimations.push_back(e);
+    e << 3, 3, 0.4, 0.3;
+    estimations.push_back(e);
+    
+    //the corresponding list of ground truth values
+    VectorXd g(4);
+    g << 1.1, 1.1, 0.3, 0.2;
+    ground_truth.push_back(g);
+    g << 2.1, 2.1, 0.4, 0.3;
+    ground_truth.push_back(g);
+    g << 3.1, 3.1, 0.5, 0.4;
+    ground_truth.push_back(g);
+    
+    //call the CalculateRMSE and print out the result
+    cout << calculateRMSE(estimations, ground_truth) << endl;
     
     return 0;
+}
+
+VectorXd calculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+    
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if(estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+        cout << "calculateRMSE() - Error - Invalid Estimations vector size" << endl;
+        return rmse;
+    }
+    
+    //accumulate squared residuals
+    VectorXd sum(4);
+    sum << 0, 0, 0, 0;
+    
+    for(int i = 0; i < estimations.size(); ++i) {
+        VectorXd estimated = estimations[i];
+        VectorXd actual = ground_truth[i];
+        
+        VectorXd diff = (estimated - actual);
+        //coefficient-wise multiplication
+        diff = diff.array() * diff.array();
+        sum = sum + diff;
+    }
+    
+    //calculate the mean
+    Eigen::VectorXd mean = sum / estimations.size();
+    
+    //calculate the squared root
+    rmse = mean.array().sqrt();
+    
+    return rmse;
 }
 
 Eigen::MatrixXd calculateJacobian(const Eigen::VectorXd& x_state) {
